7fa419af1318f84b06f0e87c69986c943c727599
Ahmednuur <xararavic1547@gmail.com>
2024-04-20 01:57:56 -0700

    new file

---
+++ dark.js
@@ -0,0 +1,35 @@
+let icon = document.getElementById("flexSwitchCheckDefault123");
+let style = document.getElementById("style");
+
+icon.onclick = function(){
+  // style.href = ""
+  // style.href = "white.css"
+  
+
+  if(style.href.includes("dark.css")){
+    style.href = "white.css"
+    // icon.src = "icons/moonn-01-01.png"
+  } else if (style.href.includes("white.css")) {
+    style.href = "dark.css"
+    // icon.src = "icons/sunn-01.png"
+    
+  }
+  // style.src ="darktheme.css"
+  // document.body.classList.toggle("darkmode");
+  // if(document.body.classList.contains("darkmode")){
+  //   icon.src = "icons/sunn-01.png"
+  // } else {
+  //   icon.src = "icons/moonn-01-01.png"
+  // }
+ 
+}
+ 
+// style theme
+// let style = document.getElementById("style");
+
+// students record
+$(document).ready(function(){
+  let marks; 
+});
+
+
