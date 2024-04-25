ad9218601e5422a674b45824b3a05c92b5bdc363
Ahmednuur <xararavic1547@gmail.com>
2024-04-25 03:29:04 -0700

    code added

--- dark.js
+++ dark.js
@@ -1,26 +1,23 @@
-let icon = document.getElementById("flexSwitchCheckDefault");
+let icon = document.getElementById("togle");
 let style = document.getElementById("style");
+let togleinner = document.getElementById("togle-inner")
 
 icon.onclick = function(){
   // style.href = ""
-  style.href = "white.css"
+  
   
 
   if(style.href.includes("dark.css")){
     style.href = "white.css"
-    // icon.src = "icons/moonn-01-01.png"
+togleinner.innerText="dark"
+   
   } else if (style.href.includes("white.css")) {
     style.href = "dark.css"
-    // icon.src = "icons/sunn-01.png"
+    togleinner.innerText="white"
+  
     
   }
-  // style.src ="darktheme.css"
-  // document.body.classList.toggle("darkmode");
-  // if(document.body.classList.contains("darkmode")){
-  //   icon.src = "icons/sunn-01.png"
-  // } else {
-  //   icon.src = "icons/moonn-01-01.png"
-  // }
+
  
 }
 $(document).ready(function(){
